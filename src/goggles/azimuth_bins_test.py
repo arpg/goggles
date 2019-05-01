ddfa16ff8243a5e498de3812571706eed8db4a74
cstahoviak <cast9875@colorado.edu>
2019-05-01 11:09:07 -0600

    first committed code; ransac performace does not match matlab; covariance to be implemented

---
+++ src/goggles/azimuth_bins_test.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+import rospy
+import numpy as np
+import scipy as sp
+
+from goggles.radar_utilities import RadarUtilities
+
+def main():
+    radar_azimuth_bins = np.genfromtxt('1642_azimuth_bins.csv', delimiter=',')
+    # print(radar_azimuth_bins)
+
+    utils = RadarUtilities()
+
+    numAzimuthBins = utils.getNumAzimuthBins( radar_azimuth_bins )
+    print(numAzimuthBins)
+
+
+if __name__ == '__main__':
+    main()
