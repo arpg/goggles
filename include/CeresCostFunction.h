c80a3d332693e6f5fb73b7a4c69d9eb2eb8fb069
aKramer <1988kramer@gmail.com>
2019-08-01 12:22:08 -0600

    further development to imu cost function

--- include/CeresCostFunction.h
+++ include/CeresCostFunction.h
@@ -7,6 +7,16 @@ struct ImuMeasurement
 	double t_;          // timestamp
 }
 
+struct ImuParams
+{
+	double g_; // gravity magnitude
+	double sigma_g_; // gyro noise prior
+	double sigma_a_; // accelerometer noise prior
+	double sigma_b_g_; // gyro bias noise prior
+	double sigma_b_a_; // accelerometer bias noise prior
+	double b_a_tau_; // accelerometer bias random walk parameter
+}
+
 class BodyVelocityCostFunction : public ceres::CostFunction
 {
   public:
@@ -163,25 +173,99 @@ class ImuVelocityCostFunction : public ceres::CostFunction
 {
 
 	typedef std::vector<double> ImuState;
+
   public:
 		ImuVelocityCostFunction(double t0, 
 														double t1, 
-														std::vector<ImuMeasurement> &imu_measurements)
-			: t0_(t0), t1_(t1), imu_measurements_(imu_measurements)
+														std::vector<ImuMeasurement> &imu_measurements,
+														ImuParams &params)
+			: t0_(t0), t1_(t1), imu_measurements_(imu_measurements), params_(params)
 		{
-			set_num_residuals(3);
-			mutable_parameter_block_sizes()->push_back(3); // orientation at t0
+			set_num_residuals(12);
+			mutable_parameter_block_sizes()->push_back(4); // orientation at t0 (i,j,k,w)
 			mutable_parameter_block_sizes()->push_back(3); // velocity at t0
-			mutable_parameter_block_sizes()->push_back(3); // orientation at t1
+			mutable_parameter_block_sizes()->push_back(3); // imu gyro biases at t0
+			mutable_parameter_block_sizes()->push_back(3); // imu accelerometer biases at t0
+			mutable_parameter_block_sizes()->push_back(4); // orientation at t1
 			mutable_parameter_block_sizes()->push_back(3); // velocity at t1
-			mutable_parameter_block_sizes()->push_back(3); // imu gyro biases
-			mutabel_parameter_block_sizes()->push_back(3); // imu accelerometer biases
+			mutable_parameter_block_sizes()->push_back(3); // imu gyro biases at t1
+			mutabel_parameter_block_sizes()->push_back(3); // imu accelerometer biases at t1
 		}
 
 		bool Evaluate(double const* const* parameters,
 									double* residuals,
 									double** jacobians) const
 		{
+			// map parameter blocks to eigen containers
+			Eigen::Map<const Eigen::Vector4d> q_ws_0(&parameters[0][0]);
+			Eigen::Map<const Eigen::Vector3d> v_s_0(&parameters[1][0]);
+			Eigen::Map<const Eigen::Vector3d> b_g_0(&parameters[2][0]);
+			Eigen::Map<const Eigen::Vector3d> b_a_0(&parameters[3][0]);
+			Eigen::Map<const Eigen::Vector4d> q_ws_1(&parameters[4][0]);
+			Eigen::Map<const Eigen::Vector3d> v_s_1(&parameters[5][0]);
+			Eigen::Map<const Eigen::Vector3d> b_g_1(&parameters[6][0]);
+			Eigen::Map<const Eigen::Vector3d> b_a_1(&parameters[7][0]);
+
+			// get rotation matrices
+			const Eigen::Quaterniond q_ws_0_quat(q_ws_0(3), q_ws_0(0), q_ws_0(1), q_ws_0(2));
+			const Eigen::Quaterniond q_ws_1_quat(q_ws_1(3), q_ws_1(0), q_ws_1(1), q_ws_1(2));
+			const Eigen::Matrix3d C_ws_0 = q_ws_0_quat.toRotationMatrix();
+			const Eigen::Matrix3d C_sw_0 = C_ws_0.inverse();
+			const Eigen::Matrix3d C_ws_1 = q_ws_1_quat.toRotationMatrex();
+			const Eigen::Matrix3d C_sw_1 = C_ws_1.inverse();
+
+			// initialize propagated states
+			Eigen::Vector4d q_ws_hat(q_ws_0(0), q_ws_0(1), q_ws_0(2), q_ws_0(3));
+			Eigen::Vector3d v_s_hat(v_s_0(0), v_s(1), v_s(2));
+			Eigen::Vector3d b_a_hat(b_a(0), b_a(1), b_a(2));
+
+			Eigen::Matrix<double,12,12> F; // jacobian matrix
+			Eigen::Matrix<double,12,12> P; // covariance matrix
+
+			F.setZero();
+			P.setZero();
+
+			// propagate imu measurements, tracking jacobians and covariance
+			for (int i = 1; i < imu_measurements_.size(); i++)
+			{
+				ImuMeasurement meas_0 = imu_measurements_[i-1];
+				ImuMeasurement meas_1 = imu_measurements_[i];
+
+				// if meas_0 is before t0_, interpolate measurement to match t0_
+				if (meas_0.t_ < t0_)
+				{
+					double c = (t0_ - meas_0.t_) / (meas_1.t_ - meas_0.t_);
+					meas_0.t_ = t0_;
+					meas_0.g_ = (1.0 - c) * meas_0.g_ + c * meas_1.g_;
+					meas_0.a_ = (1.0 - c) * meas_0.a_ + c * meas_1.a_;
+				}
+
+				// if meas_1 is after t1_, interpolate measurement to match t1_
+				if (meas_1.t_ > t1_)
+				{
+					double c = (t1_ - meas_0.t_) / (meas_1.t_ - meas_0.t_);
+					meas_1.t_ = t1_;
+					meas_1.g_ = (1.0 - c) * meas_0.g_ + c * meas_1.g_;
+					meas_1.a_ = (1.0 - c) * meas_0.a_ + c * meas_1.a_;
+				}
+
+				// integrate measurements using Runge-Kutta 4
+				std::vector<double> x0;
+				for (int j = 0; j < 4; j++) x0.push_back(q_ws_hat(j));
+				for (int j = 0; j < 3; j++) x0.push_back(v_s_hat(j));
+				for (int j = 0; j < 3; j++) x0.push_back(b_g(j));
+				for (int j = 0; j < 3; j++) x0.push_back(b_a(j));
+				double dt = (meas_1.t_ - meas_0.t_) / 10.0;
+				ImuIntegrator imu_int(meas_0, meas_1, params_.g_, params.b_a_tau_);
+				boost::odeint::runge_kutta4<std::vector<double>> stepper;
+				boost::odeint::integrate_const(stepper, imu_int, x0, meas_0.t_, meas_1.t_, dt);
+				q_ws_hat << x0[0], x0[1], x0[2], x0[3];
+				v_s_hat << x0[4], x0[5], x0[6];
+				b_a_hat << x0[10], x0[11], x0[12];
+
+				// update covariance and jacobians
+			}
+			
 			return true;
 		}
 
@@ -189,5 +273,5 @@ class ImuVelocityCostFunction : public ceres::CostFunction
 		double t0_;
 		double t1_;
 		std::vector<ImuMeasurement> imu_measurements_;
-		
+		ImuParams params_;
 };
