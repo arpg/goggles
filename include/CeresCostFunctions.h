c52f5e7a267fe83260bd0c351fec5d14f074f5f5
Andrew Kramer <1988kramer@gmail.com>
2019-08-01 15:00:55 -0600

    got imu cost function to build, not completed or tested yet, however

--- include/CeresCostFunctions.h
+++ include/CeresCostFunctions.h
@@ -5,7 +5,7 @@ struct ImuMeasurement
 	Eigen::Vector3d g_; // gyro reading
 	Eigen::Vector3d a_; // accelerometer reading
 	double t_;          // timestamp
-}
+};
 
 struct ImuParams
 {
@@ -15,7 +15,7 @@ struct ImuParams
 	double sigma_b_g_; // gyro bias noise prior
 	double sigma_b_a_; // accelerometer bias noise prior
 	double b_a_tau_; // accelerometer bias random walk parameter
-}
+};
 
 class BodyVelocityCostFunction : public ceres::CostFunction
 {
@@ -109,26 +109,26 @@ class VelocityChangeCostFunction : public ceres::CostFunction
 class ImuIntegrator
 {
 	public:
-		PropagateImuIntegrator(ImuMeasurement &m0, ImuMeasurement &m1, double g_w, double tau)
+		ImuIntegrator(ImuMeasurement &m0, ImuMeasurement &m1, double g_w, double tau)
 			: m0_(m0), m1_(m1), tau_(tau)
 		{
 			g_w_ << 0,0,g_w; // define world gravity as in positive Z direction
 		}
 		
-		void operator(const std::vector<double> &x, 
+		void operator() (const std::vector<double> &x, 
 									std::vector<double> &dxdt, 
 									const double t)
 		{
 			// map in and out states to Eigen datatypes for convenience
-			Eigen::Map<Eigen::Vector4d> q_ws(x.data()[0]); // sensor-to-world rotation
+			Eigen::Map<const Eigen::Vector4d> q_ws(&x[0]); // sensor-to-world rotation
 																										 // assumes i,j,k,w representation
-			Eigen::Map<Eigen::Vector3d> v_s(x.data()[4]); // sensor frame velocity
-			Eigen::Map<Eigen::Vector3d> b_g(x.data()[7]); // gyro biases
-			Eigen::Map<Eigen::Vector3d> b_a(x.data()[10]); // accelerometer biases
-			Eigen::Map<Eigen::Vector4d> q_ws_dot(dxdt.data()[0]); 
-			Eigen::Map<Eigen::Vector3d> v_s_dot(dxdt.data()[4];
-			Eigen::Map<Eigen::Vector3d> b_g_dot(dxdt.data()[7]);
-			Eigen::Map<Eigen::Vector3d> b_a_dot(dxdt.data()[10]);
+			Eigen::Map<const Eigen::Vector3d> v_s(&x[4]); // sensor frame velocity
+			Eigen::Map<const Eigen::Vector3d> b_g(&x[7]); // gyro biases
+			Eigen::Map<const Eigen::Vector3d> b_a(&x[10]); // accelerometer biases
+			Eigen::Map<Eigen::Vector4d> q_ws_dot(&dxdt[0]); 
+			Eigen::Map<Eigen::Vector3d> v_s_dot(&dxdt[4]);
+			Eigen::Map<Eigen::Vector3d> b_g_dot(&dxdt[7]);
+			Eigen::Map<Eigen::Vector3d> b_a_dot(&dxdt[10]);
 		
 			// get interpolated imu measurement at time t
 			double t0 = m0_.t_;
@@ -158,7 +158,7 @@ class ImuIntegrator
     	              -g(1),   g(0),    0;
 			v_s_dot = a + (C_sw * g_w_) - (omega_cross * v_s);
 	
-			b_g_dot = 0;
+			b_g_dot = Eigen::Vector3d::Zero();
 			b_a_dot = -(1.0 / tau_) * b_a;	
 		}
 
@@ -189,7 +189,7 @@ class ImuVelocityCostFunction : public ceres::CostFunction
 			mutable_parameter_block_sizes()->push_back(4); // orientation at t1
 			mutable_parameter_block_sizes()->push_back(3); // velocity at t1
 			mutable_parameter_block_sizes()->push_back(3); // imu gyro biases at t1
-			mutabel_parameter_block_sizes()->push_back(3); // imu accelerometer biases at t1
+			mutable_parameter_block_sizes()->push_back(3); // imu accelerometer biases at t1
 		}
 
 		bool Evaluate(double const* const* parameters,
@@ -211,13 +211,13 @@ class ImuVelocityCostFunction : public ceres::CostFunction
 			const Eigen::Quaterniond q_ws_1_quat(q_ws_1(3), q_ws_1(0), q_ws_1(1), q_ws_1(2));
 			const Eigen::Matrix3d C_ws_0 = q_ws_0_quat.toRotationMatrix();
 			const Eigen::Matrix3d C_sw_0 = C_ws_0.inverse();
-			const Eigen::Matrix3d C_ws_1 = q_ws_1_quat.toRotationMatrex();
+			const Eigen::Matrix3d C_ws_1 = q_ws_1_quat.toRotationMatrix();
 			const Eigen::Matrix3d C_sw_1 = C_ws_1.inverse();
 
 			// initialize propagated states
 			Eigen::Vector4d q_ws_hat(q_ws_0(0), q_ws_0(1), q_ws_0(2), q_ws_0(3));
-			Eigen::Vector3d v_s_hat(v_s_0(0), v_s(1), v_s(2));
-			Eigen::Vector3d b_a_hat(b_a(0), b_a(1), b_a(2));
+			Eigen::Vector3d v_s_hat(v_s_0(0), v_s_0(1), v_s_0(2));
+			Eigen::Vector3d b_a_hat(b_a_0(0), b_a_0(1), b_a_0(2));
 
 			Eigen::Matrix<double,12,12> F; // jacobian matrix
 			Eigen::Matrix<double,12,12> P; // covariance matrix
@@ -253,12 +253,12 @@ class ImuVelocityCostFunction : public ceres::CostFunction
 				std::vector<double> x0;
 				for (int j = 0; j < 4; j++) x0.push_back(q_ws_hat(j));
 				for (int j = 0; j < 3; j++) x0.push_back(v_s_hat(j));
-				for (int j = 0; j < 3; j++) x0.push_back(b_g(j));
-				for (int j = 0; j < 3; j++) x0.push_back(b_a(j));
+				for (int j = 0; j < 3; j++) x0.push_back(b_g_0(j));
+				for (int j = 0; j < 3; j++) x0.push_back(b_a_hat(j));
 				double dt = (meas_1.t_ - meas_0.t_) / 10.0;
-				ImuIntegrator imu_int(meas_0, meas_1, params_.g_, params.b_a_tau_);
-				boost::odeint::runge_kutta4<std::vector<double>> stepper;
-				boost::odeint::integrate_const(stepper, imu_int, x0, meas_0.t_, meas_1.t_, dt);
+				ImuIntegrator imu_int(meas_0, meas_1, params_.g_, params_.b_a_tau_);
+				boost::numeric::odeint::runge_kutta4<std::vector<double>> stepper;
+				boost::numeric::odeint::integrate_const(stepper, imu_int, x0, meas_0.t_, meas_1.t_, dt);
 				q_ws_hat << x0[0], x0[1], x0[2], x0[3];
 				v_s_hat << x0[4], x0[5], x0[6];
 				b_a_hat << x0[10], x0[11], x0[12];
