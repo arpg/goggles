bf1c752b2b5a42600b727b7037a978b1028b99a5
aKramer <1988kramer@gmail.com>
2019-08-20 16:05:27 -0600

    finished writing, not yet tested

--- include/QuaternionParameterization.h
+++ include/QuaternionParameterization.h
@@ -45,7 +45,17 @@ class QuaternionParameterization : public ceres::LocalParemeterization
 		bool minus(const double* x, const double* x_plus_delta,
 							 double* delta)
 		{
-			
+			// put inputs into quaternion form
+			const Eigen::Quaterniond q_plus_delta(x_plus_delta[3], x_plus_delta[0],
+																						x_plus_delta[1], x_plus_delta[2]);
+			const Eigen::Quaterniond q(x[3], x[0], x[1], x[2]);
+
+			// map output 
+			Eigen::Map<Eigen::Vector3d> delta_q_(delta);
+
+			// find quaternion difference and return twice the imaginary part
+			delta_q = 2.0 * (q_plus_delta * q.inverse()).coeffs().template head<3>();
+			return true;
 		}
 
 		bool ComputeLiftJacobian(const double* x, double* jacobian) const
@@ -55,12 +65,30 @@ class QuaternionParameterization : public ceres::LocalParemeterization
 
 		bool plusJacobian(const double* x, double* jacobian)
 		{
+			Eigen::Map<Eigen::Matrix<double,4,3,Eigen::RowMajor>> J(jacobian);
+			J.setZero();
 
+			Eigen::Quaterniond q(x[3], x[0], x[1], x[2]);
+			
+			Eigen::Matrix<double,4,3> S;
+			S.topLeftCorner<3,3>() = Eigen::Matrix3d::Identity() * 0.5;
+			J = oplus(q) * S;
+
+			return true;
 		}
 
 		bool liftJacobian(const double* x, double* jacobian)
 		{
+			Eigen::Map<Eigen::Matrix<double,3,4,Eigen::RowMajor>> J_lift(jacobian);
+			J_lift.setZero();
+			const Eigen::Quaterniond q_inv(x[3], -x[0], -x[1], -x[2]);
+			Eigen::Matrix4d Qplus = oplus(q_inv);
+			Eigen::Matrix<double,3,4> Jq_pinv;
+			Jq_pinv.bottomRightCorner<3,1>().setZero();
+			Jq_pinv.topLeftCorner<3,3>() = Eigen::Matrix3d::Identity() * 2.0;
+			J_lift = Jq_pinv * Qplus;
 
+			return true;
 		}
 
 		bool ComputeJacobian(const double* x, double* jacobian) const
@@ -72,7 +100,24 @@ class QuaternionParameterization : public ceres::LocalParemeterization
 															 double* jacobian,
 															 double* jacobian_num_diff)
 		{
-
+			plusJacobian(x, jacobian);
+			Eigen::Map<Eigen::Matrix<double,4,3,Eigen::RowMajor>> Jp(jacobian);
+			Eigen::Map<Eigen::Matrix<double,4,3,Eigen::RowMajor>> Jpn(jacobian_num_diff);
+			double dx = 1.0e-9;
+			Eigen::Vector4d xp;
+			Eigen::Vector4d xm;
+			for (size_t i = 0; i < 3; i++)
+			{
+				Eigen::Vector3d delta;
+				delta.setZero();
+				delta[i] = dx;
+				Plus(x, delta.data(), xp.data());
+				delta[i] = -dx;
+				Plus(x, delta.data(), xm.data());
+				Jpn.col(i) = (xp - xm) / (2.0 * dx);
+			}
+			if ((Jp - Jpn).norm() < 1.0e-6) return true;
+			else return false;
 		}
 
 		bool Verify(const double* x_raw, double perturbation_magnitude) const
@@ -154,5 +199,18 @@ class QuaternionParameterization : public ceres::LocalParemeterization
 				const double x_6 = x_2 * x_2 * x_2;
 				return 1.0 - c_2 * x_2 + c_4 * x_4 - c_6 * x_6;
 			}
+		}
+
+		Eigen::Matrix4d oplus(const Eigen::Quaterniond &q_BC)
+		{
+			Eigen::Vector4d q = q_BC.coeffs();
+			Eigen::Matrix4d Q;
+
+			Q(0,0) =  q[3]; Q(0,1) =  q[2]; Q(0,2) = -q[1]; Q(0,3) =  q[0];
+			Q(1,0) = -q[2]; Q(1,1) =  q[3]; Q(1,2) =  q[0]; Q(1,3) =  q[1];
+			Q(2,0) =  q[1]; Q(2,1) = -q[0]; Q(2,2) =  q[3]; Q(2,3) =  q[2];
+			Q(3,0) = -q[0]; Q(3,1) = -q[1]; Q(3,2) = -q[2]; Q(3,3) =  q[3];
+		
+			return Q;
 		}
 };
