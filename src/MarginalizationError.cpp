4244c4d032d07af9c3cc51ae7526b665bb9fc1fc
Andrew Kramer <1988kramer@gmail.com>
2019-09-26 15:39:33 -0600

    started on implementation

--- src/MarginalizationError.cpp
+++ src/MarginalizationError.cpp
@@ -1,1 +1,34 @@
 #include <MarginalizationError.h>
+
+MarginalizationError::MarginalizationError()
+{
+
+}
+
+MarginalizationError::~MarginalizationError(){}
+
+bool MarginalizationError::AddResidualBlocks(
+  const std::vector<ceres::ResidualBlockId> &residual_block_ids)
+{
+
+}
+
+bool MarginalizationError::AddResidualBlock(
+  ceres::ResidualBlockId residual_block_id)
+{
+
+}
+
+bool MarginalizationError::MarginalizeOut(
+  const std::vector<uint64_t> & parameter_block_ids)
+{
+
+}
+
+bool MarginalizationError::Evaluate(
+  double const* const* parameters,
+  double* residuals,
+  double** jacobians) const
+{
+  
+}
