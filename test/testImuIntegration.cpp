9c674ef571ba07bd42d4d54c3c2b4b010b03326f
Andrew Kramer <1988kramer@gmail.com>
2019-10-09 10:24:35 -0600

    initialized google logging

--- test/testImuIntegration.cpp
+++ test/testImuIntegration.cpp
@@ -465,6 +465,7 @@ TEST(goggleTests, ImuIntegration)
 
 int main(int argc, char **argv)
 {
+  google::InitGoogleLogging(argv[0]);
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
