c7acc3b4fe954f8468dc328b25c3d433b65592f2
Andrew Kramer <1988kramer@gmail.com>
2019-10-31 10:17:14 -0600

    added id provider class

---
+++ include/IdProvider.h
@@ -0,0 +1,38 @@
+/**
+  * This code borrows heavily from IdProvider.hpp in OKVIS:
+  * https://github.com/ethz-asl/okvis/blob/master/okvis_ceres/include/okvis/ceres/ImuError.hpp
+  * persuant to the following copyright:
+  *
+  *  OKVIS - Open Keyframe-based Visual-Inertial SLAM
+  *  Copyright (c) 2015, Autonomous Systems Lab / ETH Zurich
+  *
+  *  Redistribution and use in source and binary forms, with or without
+  *  modification, are permitted provided that the following conditions are met:
+  * 
+      * Redistributions of source code must retain the above copyright notice,
+        this list of conditions and the following disclaimer.
+      * Redistributions in binary form must reproduce the above copyright notice,
+        this list of conditions and the following disclaimer in the documentation
+        and/or other materials provided with the distribution.
+      * Neither the name of Autonomous Systems Lab / ETH Zurich nor the names of
+        its contributors may be used to endorse or promote products derived from
+        this software without specific prior written permission.
+  */
+
+#include <IdProvider.h>
+
+namespace IdProvider
+{
+  std::atomic<uint64_t> _id(0);
+
+  uint64_t instance::NewId()
+  {
+    const uint64_t new_id = ++id;
+    return new_id;
+  }
+
+  uint64_t instance::CurrentId()
+  {
+    return _id;
+  }
+}
---
+++ src/IdProvider.cpp
@@ -0,0 +1,39 @@
+/**
+  * This code borrows heavily from IdProvider.cpp in OKVIS:
+  * https://github.com/ethz-asl/okvis/blob/master/okvis_ceres/include/okvis/ceres/ImuError.hpp
+  * persuant to the following copyright:
+  *
+  *  OKVIS - Open Keyframe-based Visual-Inertial SLAM
+  *  Copyright (c) 2015, Autonomous Systems Lab / ETH Zurich
+  *
+  *  Redistribution and use in source and binary forms, with or without
+  *  modification, are permitted provided that the following conditions are met:
+  * 
+      * Redistributions of source code must retain the above copyright notice,
+        this list of conditions and the following disclaimer.
+      * Redistributions in binary form must reproduce the above copyright notice,
+        this list of conditions and the following disclaimer in the documentation
+        and/or other materials provided with the distribution.
+      * Neither the name of Autonomous Systems Lab / ETH Zurich nor the names of
+        its contributors may be used to endorse or promote products derived from
+        this software without specific prior written permission.
+  */
+
+#pragma once
+#ifndef IDPROVIDER_H_
+#define IDPROVIDER_H_
+
+#include <stdint.h>
+#include <iostream>
+#include <atomic>
+
+namespace IdProvider 
+{
+  struct instance
+  {
+    static uint64_t NewId();
+    static uint64_t CurrentId();
+  };
+}
+
+#endif
