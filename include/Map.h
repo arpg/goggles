37ea70d431d12d2f5f7364e70c35ea9f7a2676c4
Andrew Kramer <1988kramer@gmail.com>
2019-10-31 10:17:00 -0600

    added citation to okvis class

--- include/Map.h
+++ include/Map.h
@@ -1,3 +1,24 @@
+/**
+  * This code borrows heavily from Map.hpp in OKVIS:
+  * https://github.com/ethz-asl/okvis/blob/master/okvis_ceres/include/okvis/ceres/ImuError.hpp
+  * persuant to the following copyright:
+  *
+  *  OKVIS - Open Keyframe-based Visual-Inertial SLAM
+  *  Copyright (c) 2015, Autonomous Systems Lab / ETH Zurich
+  *
+  *  Redistribution and use in source and binary forms, with or without
+  *  modification, are permitted provided that the following conditions are met:
+  * 
+      * Redistributions of source code must retain the above copyright notice,
+        this list of conditions and the following disclaimer.
+      * Redistributions in binary form must reproduce the above copyright notice,
+        this list of conditions and the following disclaimer in the documentation
+        and/or other materials provided with the distribution.
+      * Neither the name of Autonomous Systems Lab / ETH Zurich nor the names of
+        its contributors may be used to endorse or promote products derived from
+        this software without specific prior written permission.
+  */
+
 #pragma once
 #ifndef MAP_H_
 #define MAP_H_
