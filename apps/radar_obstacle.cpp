c753469b305074ecc80dc63685cf4caf9f343981
Andrew Kramer <1988kramer@gmail.com>
2019-11-25 18:33:02 -0700

    not sure

--- apps/radar_obstacle.cpp
+++ apps/radar_obstacle.cpp
@@ -1,7 +1,11 @@
+#define PCL_NO_PRECOMPILE
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl_ros/point_cloud.h>
 #include <pcl/point_types.h>
+#include <pcl/segmentation/extract_clusters.h>
+#include <pcl/kdtree/kdtree.h>
+#include <pcl/filters/extract_indices.h>
 #include <Eigen/Core>
 #include <glog/logging.h>
 #include <DataTypes.h>
@@ -22,35 +26,9 @@ public:
   /// \brief Default constructor
   /// \param[in] nh a ros nodehandle
   RadarObstacleFilter(ros::NodeHandle nh) 
-  : nh_(nh),
-    max_range_(std::numeric_limits<double>::max()), 
-    min_range_(0.0)
+  : nh_(nh)
   {
     InitializeNode();
-    MakeRayLookupTable();
-  }
-
-  /// \brief Makes a lookup table of rays for each index of elevation and azimuth
-  void MakeRayLookupTable()
-  {
-    std::vector<Eigen::Vector3d> init_list;
-    ray_table_.resize(num_azimuth_bins_);
-    Eigen::Vector3d init_vec = Eigen::Vector3d::Zero();
-    for (int i = 0; i < num_azimuth_bins_; i++)
-      ray_table_[i].resize(num_elevation_bins_);
-
-    for (int i = 0; i < num_azimuth_bins_; i++)
-    {
-      for (int j = 0; j < num_elevation_bins_; j++)
-      {
-        double el_angle = double(j) * bin_width_ - elevation_fov_;
-        double az_angle = double(i) * bin_width_ - azimuth_fov_;
-        Eigen::Vector3d ray(cos(az_angle) * cos(el_angle),
-                            sin(az_angle) * cos(el_angle),
-                            sin(el_angle));
-        ray_table_[i][j] = ray;
-      }
-    }
   }
 
   /// \brief Initializes ros node subscriber and publisher
@@ -60,31 +38,9 @@ public:
     std::string out_topic;
     nh_.getParam("input_cloud", in_topic);
     nh_.getParam("output_cloud", out_topic);
-    nh_.getParam("min_range", min_range_);
-    nh_.getParam("max_range", max_range_);
-    nh_.getParam("azimuth_fov", azimuth_fov_);
-    nh_.getParam("elevation_fov", elevation_fov_);
     nh_.getParam("bin_width", bin_width_);
 
-    // convert degrees to radians
-    azimuth_fov_ *= M_PI / 180.0;
-    elevation_fov_ *= M_PI / 180.0;
     bin_width_ *= M_PI / 180.0;
-
-    // get num bins
-    num_azimuth_bins_ = size_t(2.0 * azimuth_fov_ / bin_width_);
-    num_elevation_bins_ = size_t(2.0 * elevation_fov_ / bin_width_);
-
-    binned_points_.resize(num_azimuth_bins_);
-    bin_mutexes_.resize(num_azimuth_bins_);
-    for (int i = 0; i < num_azimuth_bins_; i++)
-    {
-      binned_points_[i].resize(num_elevation_bins_);
-      for (int j = 0; j < num_elevation_bins_; j++)
-      {
-        bin_mutexes_[i].push_back(new boost::mutex());
-      }
-    }
 
     std::string ns = ros::this_node::getNamespace();
 
@@ -98,182 +54,143 @@ public:
   {
     auto start = std::chrono::high_resolution_clock::now();
     double timestamp = msg->header.stamp.toSec();
-    pcl::PointCloud<RadarPoint>::Ptr cloud(new pcl::PointCloud<RadarPoint>);
+    pcl::PointCloud<RadarPoint>::Ptr cloud(new pcl::PointCloud<RadarPoint>());
     pcl::fromROSMsg(*msg, *cloud);
-
-    for (int i = 0; i < num_azimuth_bins_; i++)
-    {
-      for (int j = 0; j < num_elevation_bins_; j++)
-        binned_points_[i][j].clear();
-    }
-
     
-    boost::asio::io_service io_service;
-    boost::thread_group pool;
-    boost::asio::io_service::work work(io_service);
-    size_t num_threads = 4;
+    // detect point clusters
+    pcl::search::KdTree<RadarPoint>::Ptr tree(new pcl::search::KdTree<RadarPoint>());
+    tree->setInputCloud(cloud);
+    std::vector<pcl::PointIndices> cluster_indices;
+    pcl::EuclideanClusterExtraction<RadarPoint> ec;
+    ec.setClusterTolerance(0.2);
+    ec.setMinClusterSize(1);
+    ec.setMaxClusterSize(20);
+    ec.setSearchMethod(tree);
+    ec.setInputCloud(cloud);
+    ec.extract(cluster_indices);
 
-    for (int i = 0; i < num_threads; i++)
-      pool.create_thread(boost::bind(&boost::asio::io_service::run, &io_service));
-    
-    // iterate over all points in the input cloud
-    for (pcl::PointCloud<RadarPoint>::iterator it = cloud->begin(); 
-          it != cloud->end(); it++)
+    pcl::PointCloud<RadarPoint>::Ptr clustered_cloud(new pcl::PointCloud<RadarPoint>());
+
+    // determine if clusters are roughly on same ray from sensor
+    // if so cluster is likely reflections
+    for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin();
+      it != cluster_indices.end(); it++)
     {
-      
-      io_service.post(boost::bind(&RadarObstacleFilter::BinRadarReturns,
-                                  this,
-                                  *it));
-      
-      //BinRadarReturns(*it);
-    }
-    io_service.stop();
-    pool.join_all();
-    
-    // if multiple returns exist in a bin, 
-    // remove all but the highest intensity return
-    for (int i = 0; i < num_azimuth_bins_; i++)
-    {
-      for (int j = 0; j < num_elevation_bins_; j++)
+      std::vector<int> indices_copy = it->indices;
+      bool replace_with_mean = false;
+      if (indices_copy.size() >= 3)
       {
-        if (binned_points_[i][j].size() > 1)
+        //LOG(ERROR) << "getting rays";
+        // get unit ray pointing toward each point in the cluster
+        std::vector<Eigen::Vector3d> rays;
+        for (std::vector<int>::const_iterator pit = it->indices.begin();
+          pit != it->indices.end(); pit++)
         {
-          std::sort(binned_points_[i][j].begin(),
-                    binned_points_[i][j].end(),
-                    compareRange);
-          while (binned_points_[i][j].size() > 1)
+          Eigen::Vector3d unit_ray(cloud->points[*pit].x,
+                                   cloud->points[*pit].y,
+                                   cloud->points[*pit].z);
+          unit_ray.normalize();
+          rays.push_back(unit_ray);
+        }
+        //LOG(ERROR) << "getting angles";
+        // determine the angle between each pair of points
+        Eigen::MatrixXd angles(rays.size(), rays.size());
+        angles.setZero();
+
+        for (int i = 0; i < rays.size(); i++)
+        {
+          for (int j = i+1; j < rays.size(); j++)
           {
-            if (binned_points_[i][j][0].intensity > binned_points_[i][j][1].intensity)
-              binned_points_[i][j].erase(binned_points_[i][j].begin() + 1);
-            else
-              binned_points_[i][j].erase(binned_points_[i][j].begin());
+            angles(i,j) = std::fabs(acos(rays[i].dot(rays[j])));
+            angles(j,i) = angles(i,j);
           }
         }
-      }
-    }
-    
-    // add fake return at max range to each empty bin
-    for (int i = 0; i < num_azimuth_bins_; i++)
-    {
-      for (int j = 0; j < num_elevation_bins_; j++)
-      {
-        if (binned_points_[i][j].size() == 0)
+        //LOG(ERROR) << "checking for outliers";
+        // remove outliers
+        int cluster_idx = 0;
+        for (int i = 0; i < angles.cols(); i++)
         {
-          RadarPoint fake_point;
-          fake_point.range = max_range_;
-          fake_point.intensity = 0.0;
-          fake_point.doppler = 0.0;
-          fake_point.x = ray_table_[i][j].x() * max_range_;
-          fake_point.y = ray_table_[i][j].y() * max_range_;
-          fake_point.z = ray_table_[i][j].z() * max_range_;
-          binned_points_[i][j].push_back(fake_point);
+          double min_angle = 10.0;
+          for (int j = 0; j < angles.rows(); j++)
+          {
+            if (i != j && angles(i,j) < min_angle) 
+              min_angle = angles(i,j);
+          }
+          if (min_angle > 2.0 * bin_width_)
+          {
+            //LOG(ERROR) << "removing outlier at " << cluster_idx << " from cluster of size " << indices_copy.size();
+            indices_copy.erase(indices_copy.begin() + cluster_idx);
+          }
+          else
+          {
+            cluster_idx++;
+          }
+        }
+        replace_with_mean = indices_copy.size() > 3;
+      }
+      // if max angle is less than bin width, add the 
+      // mean of the points to the new cloud
+      if (replace_with_mean)
+      {
+        RadarPoint mean_point;
+        mean_point.x = 0.0;
+        mean_point.y = 0.0;
+        mean_point.z = 0.0;
+        mean_point.intensity = 0.0;
+        mean_point.doppler = 0.0;
+        mean_point.range = 0.0;
+        for (std::vector<int>::const_iterator pit = indices_copy.begin();
+          pit != indices_copy.end(); pit++)
+        {
+          mean_point.x += cloud->points[*pit].x;
+          mean_point.y += cloud->points[*pit].y;
+          mean_point.z += cloud->points[*pit].z;
+          mean_point.intensity += cloud->points[*pit].intensity;
+          mean_point.doppler += cloud->points[*pit].doppler;
+          mean_point.range += cloud->points[*pit].range;
+        }
+        mean_point.x /= indices_copy.size();
+        mean_point.y /= indices_copy.size();
+        mean_point.z /= indices_copy.size();
+        mean_point.intensity /= indices_copy.size();
+        mean_point.doppler /= indices_copy.size();
+        mean_point.range /= indices_copy.size();
+
+        clustered_cloud->points.push_back(mean_point);
+      }
+      else // add the individual points to the cloud
+      {
+        for (std::vector<int>::const_iterator pit = it->indices.begin();
+          pit != it->indices.end(); pit++)
+        {
+          clustered_cloud->points.push_back(cloud->points[*pit]);
         }
       }
     }
 
-    // publish new pointcloud
-    pcl::PointCloud<RadarPoint>::Ptr out_cloud(boost::make_shared<pcl::PointCloud<RadarPoint>>());
-    for (int i = 0; i < num_azimuth_bins_; i++)
-    {
-      for (int j = 0; j < num_elevation_bins_; j++)
-      {
-        for (int k = 0; k < binned_points_[i][j].size(); k++)
-        {
-          out_cloud->push_back(binned_points_[i][j][k]);
-        }
-      }
-    }
 
     sensor_msgs::PointCloud2 out_cloud_msg;
     pcl::PCLPointCloud2 out_cloud2;
-    pcl::toPCLPointCloud2(*out_cloud, out_cloud2);
+    pcl::toPCLPointCloud2(*clustered_cloud, out_cloud2);
     pcl_conversions::fromPCL(out_cloud2, out_cloud_msg);
     out_cloud_msg.header.stamp = ros::Time(timestamp);
     out_cloud_msg.header.frame_id = msg->header.frame_id;
 
     auto finish = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed = finish - start;
-    LOG(ERROR) << "execution time: " << elapsed.count();
+    LOG(INFO) << "execution time: " << elapsed.count();
 
     pub_.publish(out_cloud_msg);
   }
 
-  void BinRadarReturns(RadarPoint& p)
-  {
-    // find range
-    Eigen::Vector3d point(p.x,p.y,p.z);
-    double range = point.norm();
-
-    if (range <= max_range_ && range >= min_range_)
-    {
-      // find azimuth bin index for the current point
-      Eigen::Vector2d az_vector(p.x, p.y);
-      az_vector.normalize();
-      double az_angle = asin(az_vector.y());
-      size_t az_idx = size_t((az_angle / bin_width_) + (double(num_azimuth_bins_ - 1) / 2.0));
-
-      // find elevation bin index for current point
-      Eigen::Vector3d el_vector(p.x, p.y, p.z);
-      el_vector.normalize();
-      double el_angle = asin(el_vector.z());
-      size_t el_idx = size_t((el_angle / bin_width_) + (double(num_elevation_bins_ - 1) / 2.0));
-
-      // add to binned points if it is within field of view
-      if (std::fabs(az_angle) <= azimuth_fov_
-          && std::fabs(el_angle) <= elevation_fov_)
-      {
-        // copy current point
-        RadarPoint new_point(p);
-
-        // adjust xyz to bin center
-        new_point.x = p.x; //ray_table_[az_idx][el_idx].x() * range;
-        new_point.y = p.y; //ray_table_[az_idx][el_idx].y() * range;
-        new_point.z = p.z; //ray_table_[az_idx][el_idx].z() * range;
-        new_point.range = range;
-
-        if (az_idx >= num_azimuth_bins_ || az_idx < 0)
-          LOG(FATAL) << "azimuth index, " << az_idx << ", outside range of bin indices, 0 to " << num_azimuth_bins_ - 1;
-
-        if (el_idx >= num_elevation_bins_ || el_idx < 0)
-        {
-          LOG(ERROR) << "elevation angle: " << el_angle << " " << el_angle / bin_width_;
-          LOG(FATAL) << "elevation index, " << el_idx << ", outside range of bin indices, 0 to " << num_elevation_bins_ - 1;
-
-        }
-
-        // add to binned points
-        boost::lock_guard<boost::mutex> lck(*bin_mutexes_[az_idx][el_idx]);
-        binned_points_[az_idx][el_idx].push_back(new_point);
-      }
-    }
-  }
-
-  
 protected:
 
-  struct 
-  {
-    bool operator()(RadarPoint a, RadarPoint b) const
-    {
-      return a.range < b.range;
-    }
-  } compareRange;
-
-  size_t num_elevation_bins_; 
-  size_t num_azimuth_bins_;
   double bin_width_; // bin width in radians
-  double max_range_;
-  double min_range_;
-  double azimuth_fov_; // 1/2 azimuth field of view in radians
-  double elevation_fov_; // 1/2 elevation field of view in radians
+  std::deque<pcl::PointCloud<RadarPoint>> cloud_queue_;
 
   ros::NodeHandle nh_;
   ros::Publisher pub_;
   ros::Subscriber sub_;
-
-  std::vector<std::vector<Eigen::Vector3d>> ray_table_;
-  std::vector<std::vector<std::vector<RadarPoint>>> binned_points_;
-  std::vector<std::vector<boost::mutex*>> bin_mutexes_;
 };
 
 
