9f5924dab63c414e70e74a4ced08b110a042f921
Andrew Kramer <1988kramer@gmail.com>
2020-03-05 11:42:11 -0700

    added multiple landmarks

--- test/testPointCluster.cpp
+++ test/testPointCluster.cpp
@@ -19,42 +19,54 @@ TEST(googleTests, testPointCluster)
   Eigen::Quaterniond q_ws = Eigen::Quaterniond::UnitRandom();
   Eigen::Vector3d t_w(0.0,0.0,0.0);
   Transformation T_WS(t_w, q_ws);
-
-  // generate landmark
-  Eigen::Vector4d h_pw(5.0,1.0,-0.5,1.0);
-
-  // get landmark in sensor frame
-  Eigen::Vector4d h_ps = T_WS.inverse().T() * h_pw;
+  int num_lmrks = 10;
+  // generate landmarks
+  std::vector<Eigen::Vector4d> h_pw;
+  std::vector<Eigen::Vector4d> h_ps;
+  for (size_t i = 0; i < num_lmrks; i++)
+  {
+    Eigen::Vector3d point = Eigen::Vector3d::Random() * 5.0;
+    h_pw.push_back(Eigen::Vector4d(point[0],point[1],point[2],1.0));
+    h_ps.push_back(T_WS.inverse().T() * h_pw[i]);
+  }
+  
 
   // generate landmark measurements with random perturbations
   std::vector<Eigen::Vector3d> targets;
-  for (int i = 0; i < 10; i++)
+  for (size_t i = 0; i < num_lmrks; i++)
   {
     Eigen::Vector3d perturbation = Eigen::Vector3d::Random() * 1.0e-3;
-    Eigen::Vector3d target = h_ps.head(3) + perturbation;
-    targets.push_back(target);
+    targets.push_back(h_ps[i].head(3) + perturbation);
   }
 
   uint64_t id = IdProvider::instance().NewId();
   std::shared_ptr<PoseParameterBlock> T_WS_est = 
     std::make_shared<PoseParameterBlock>(T_WS,id,0.0);
-  id = IdProvider::instance().NewId();
-  std::shared_ptr<HomogeneousPointParameterBlock> h_pw_est =
-    std::make_shared<HomogeneousPointParameterBlock>(h_pw,id);
+  std::vector<std::shared_ptr<HomogeneousPointParameterBlock>> h_pw_est;
+  for (size_t i = 0; i < num_lmrks; i++)
+  {
+    id = IdProvider::instance().NewId();
+    //std::shared_ptr<HomogeneousPointParameterBlock> point = 
+    h_pw_est.push_back(std::make_shared<HomogeneousPointParameterBlock>(h_pw[i],id));
+  }
 
   std::shared_ptr<Map> map_ptr = std::make_shared<Map>();
   map_ptr->AddParameterBlock(T_WS_est, Map::Parameterization::Pose);
-  map_ptr->AddParameterBlock(h_pw_est, Map::Parameterization::HomogeneousPoint);
+
+  for(size_t i = 0; i < num_lmrks; i++)
+  {
+    map_ptr->AddParameterBlock(h_pw_est[i], Map::Parameterization::HomogeneousPoint);
+  }
 
   double weight = 1.0 / double(targets.size());
-  for (size_t i = 0; i < targets.size(); i++)
+  for (size_t i = 0; i < num_lmrks; i++)
   {
     std::shared_ptr<ceres::CostFunction> point_cost_func =
       std::make_shared<PointClusterCostFunction>(targets[i], weight);
     ceres::ResidualBlockId id = map_ptr->AddResidualBlock(point_cost_func,
                                                         NULL,
                                                         T_WS_est,
-                                                        h_pw_est);
+                                                        h_pw_est[i]);
   }
 
   PointClusterCostFunction* p_cost_func = 
@@ -63,7 +75,7 @@ TEST(googleTests, testPointCluster)
   // check jacobians by finite differences
   double* parameters[2];
   parameters[0] = T_WS_est->GetParameters();
-  parameters[1] = h_pw_est->GetParameters();
+  parameters[1] = h_pw_est[0]->GetParameters();
 
   double* jacobians[2];
   Eigen::Matrix<double,3,7,Eigen::RowMajor> J0;
@@ -120,14 +132,14 @@ TEST(googleTests, testPointCluster)
     Eigen::Vector3d residuals_m;
     dp_1.setZero();
     dp_1[i] = dx;
-    Eigen::Vector4d h_pw_temp = h_pw_est->GetEstimate();
-    h_pw_est->Plus(parameters[1],dp_1.data(),parameters[1]);
+    Eigen::Vector4d h_pw_temp = h_pw_est[0]->GetEstimate();
+    h_pw_est[0]->Plus(parameters[1],dp_1.data(),parameters[1]);
     p_cost_func->Evaluate(parameters,residuals_p.data(),NULL);
-    h_pw_est->SetEstimate(h_pw_temp);
+    h_pw_est[0]->SetEstimate(h_pw_temp);
     dp_1[i] = -dx;
-    h_pw_est->Plus(parameters[1],dp_1.data(),parameters[1]);
+    h_pw_est[0]->Plus(parameters[1],dp_1.data(),parameters[1]);
     p_cost_func->Evaluate(parameters,residuals_m.data(),NULL);
-    h_pw_est->SetEstimate(h_pw_temp);
+    h_pw_est[0]->SetEstimate(h_pw_temp);
     J1_min_numDiff.col(i) = (residuals_p - residuals_m) / (2.0 * dx);
   }
   if ((J1_min - J1_min_numDiff ).norm() > jacobianTolerance)
@@ -136,7 +148,6 @@ TEST(googleTests, testPointCluster)
       << "\nuser provided J1: \n" << J1_min 
       << "\n\nnum diff J1:\n" << J1_min_numDiff << "\n\n";
   }
-
   map_ptr->Solve();
   LOG(ERROR) << map_ptr->summary.FullReport();
 
