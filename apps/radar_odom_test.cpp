5ca64099471f2bfdfb587e407185f4e7c36fe793
Andrew Kramer <1988kramer@gmail.com>
2020-03-05 13:00:18 -0700

    filling in callbacks

--- apps/radar_odom_test.cpp
+++ apps/radar_odom_test.cpp
@@ -20,6 +20,7 @@
 #include <HomogeneousPointParameterBlock.h>
 #include <HomogeneousPointParameterization.h>
 #include <PointClusterCostFunction.h>
+#include <Transformation.h>
 #include <IdProvider.h>
 #include "DataTypes.h"
 
@@ -29,10 +30,97 @@ public:
 
   ClusterOdometryTester(ros::NodeHandle nh) : map_ptr_(new Map())
   {
+    nh_ = nh;
+    std::string pcl_topic;
+    std::string odom_topic;
+    nh_.getParam("pcl_topic", pcl_topic);
+    nh_.getParam("odom_topic", odom_topic);
 
+    odom_publisher_ = nh_.advertise<nav_msgs::Odometry>("/mmWaveDataHdl/odom", 1);
+    lmrk_publisher_ = nh_.advertise<sensor_msgs::PointCloud2>(
+      "/mmWaveDataHdl/odom/landmarks",1);
+
+    odom_sub_ = nh_.subscribe(odom_topic,
+                              1,
+                              &ClusterOdometryTester::odomCallback,
+                              this);
+    pcl_sub_ = nh_.subscribe(pcl_topic,
+                             1,
+                             &ClusterOdometryTester::pclCallback,
+                             this);
+
+    window_size_ = 10;
+
+    point_loss_ = new ceres::CauchyLoss(1.0);
+    map_ptr_->options.num_threads = 4;
   }
+
+  void odomCallback(const nav_msgs::OdometryConstPtr& msg)
+  {
+    double timestamp = msg->header.stamp.toSec();
+    Eigen::Quaterniond orientation(msg->pose.pose.orientation.w,
+                                   msg->pose.pose.orientation.x,
+                                   msg->pose.pose.orientation.y,
+                                   msg->pose.pose.orientation.z);
+    Eigen::Vector3d position(msg->pose.pose.position.x,
+                             msg->pose.pose.position.y,
+                             msg->pose.pose.position.z);
+    Transformation T(position, orientation);
+    odom_buffer.push_front(std::make_pair(timestamp,T));
+  }
+
+  void pclCallback(const sensor_msgs::PointCloud2ConstPtr& msg)
+  {
+    if (!odom_buffer.empty())
+    {
+      // extract pointcloud from msg
+      double timestamp = msg->header.stamp.toSec();
+      pcl::PointCloud<RadarPoint>::Ptr cloud(new pcl::PointCloud<RadarPoint>);
+      pcl::fromROSMsg(*msg, *cloud);
+
+      // get last optimized pose, if available
+      Transformation T_WS_prev;
+      if (!poses_.empty())
+        T_WS_prev = poses_.front()->GetEstimate();
+
+      // get relative transform between current and last optimized pose
+
+      // get current pose
+
+      // transform pointcloud to world frame
+
+      // associate points in current cloud to nearest point scatterers
+
+      // create cost functions for each associated point and add to map
+
+      // add unassociated points as new scatterers
+
+      // solve problem
+
+      // remove old states and residuals
+
+      // remove old scatterers with only one observation
+    }
+  }
+
 private:
+  // ros-related objects
+  ros::NodeHandle nh_;
+  ros::Publisher odom_publisher_;
+  ros::Publisher lmrk_publisher_;
+  ros::Subscriber odom_sub_;
+  ros::Subscriber pcl_sub_;
+
+  // solver objects
   std::shared_ptr<Map> map_ptr_;
+  ceres::LossFunction *point_loss_;
+  size_t window_size_;
+
+  // state and residual containers
+  std::vector<std::shared_ptr<HomogeneousPointParameterBlock>> scatterers_;
+  std::deque<std::shared_ptr<PoseParameterBlock>> poses_;
+  std::deque<std::vector<ceres::ResidualBlockId>> residual_blks_;
+  std::deque<std::pair<double,Transformation>> odom_buffer;
 };
 
 int main(int argc, char** argv)
